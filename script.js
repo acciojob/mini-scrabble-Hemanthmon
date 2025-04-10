723afb5a6c58f7ce65451832d88c7fbe9d91bf93
Hemanth V <153289923+Hemanthmon@users.noreply.github.com>
2025-04-10 19:59:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+ 
+
+	
+let inputBox = document.getElementById('evaluatedText')
+let letterCount = document.getElementById('letterCount')
+
+inputBox.addEventListener('input', () => {
+	letterCount.innerText = inputBox.value.length
+})
